70adf377e7a9ade56d98678c4f2486e76647e772
Johannus Vogel <johannus.vogel@gmail.com>
2017-12-30 02:09:49 -0600

    Adding Webpack and Vue, experimenting

---
+++ components/app.vue
@@ -0,0 +1,173 @@
+<template>
+
+  <header class="panel-cover" style="background-image: url(/images/cover.jpg)">
+
+    <!-- <div id="dd">
+      <canvas id="myCanvas" style="border:1px solid #c3c3c3;">
+            Your browser does not support the canvas element.
+      </canvas>
+    </div> -->
+    <div class="panel-main">
+
+      <div class="panel-main__inner panel-inverted">
+      <div class="panel-main__content">
+          <a href="/" title="link to home of Johannus Vogel">
+            <img src="/images/logo.png" class="user-image" alt="My Profile Photo">
+            <h1 class="panel-cover__title panel-title">Johannus Vogel</h1>
+          </a>
+          <hr class="panel-cover__divider">
+          <p class="panel-cover__description">Software engineer, artist, entrepreneur and internet poet</p>
+          <hr class="panel-cover__divider panel-cover__divider--secondary">
+
+          <div class="navigation-wrapper">
+            <transition v-on:enter="enter" appear>
+                <nav class="cover-navigation cover-navigation--primary">
+                  <ul class="navigation">
+                    <li class="navigation__item"><a href="/#blog" title="link to Johannus' blog" class="blog-button">Blog</a></li>
+                  </ul>
+                </nav>
+            </transition>
+
+            <nav class="cover-navigation navigation--social">
+              <ul class="navigation">
+
+
+                <!-- Twitter -->
+                <li class="navigation__item">
+                  <a href="http://twitter.com/johannus.vogel" title="@johannus.vogel on Twitter" target="_blank">
+                    <i class="icon icon-social-twitter"></i>
+                    <span class="label">Twitter</span>
+                  </a>
+                </li>
+
+                <!-- Facebook -->
+                <li class="navigation__item">
+                  <a href="http://fb.me/johannus.vogel" title="johannus.vogel on Facebook" target="_blank">
+                    <i class="icon icon-social-facebook"></i>
+                    <span class="label">Facebook</span>
+                  </a>
+                </li>
+
+
+
+                <!-- LinkedIn -->
+                <li class="navigation__item">
+                  <a href="https://www.linkedin.com/in/johannusvogel" title="johannusvogel on LinkedIn" target="_blank">
+                    <i class="icon icon-social-linkedin"></i>
+                    <span class="label">LinkedIn</span>
+                  </a>
+                </li>
+
+
+
+                <!-- GitHub -->
+                <li class="navigation__item">
+                  <a href="https://www.github.com/javogel" title="javogel on GitHub" target="_blank">
+                    <i class="icon icon-social-github"></i>
+                    <span class="label">GitHub</span>
+                  </a>
+                </li>
+
+
+
+                <!-- Email -->
+                <li class="navigation__item">
+                  <a href="mailto:johannus.vogel@gmail.com" title="Email johannus.vogel@gmail.com" target="_blank">
+                    <i class="icon icon-mail"></i>
+                    <span class="label">Email</span>
+                  </a>
+                </li>
+
+
+              <!-- RSS -->
+              <li class="navigation__item">
+                <a href="/feed.xml" title="Subscribe" target="_blank">
+                  <i class="icon icon-rss"></i>
+                  <span class="label">RSS</span>
+                </a>
+              </li>
+
+              </ul>
+            </nav>
+
+          </div>
+
+        </div>
+
+      </div>
+
+      <div class="panel-cover--overlay"></div>
+    </div>
+  </header>
+</template>
+
+
+<!-- <template>
+  <div class="copy">
+    <h2>{{ siteData.title }}</h2>
+    <p v-html="rawdescription"></p>
+    <h2>The Best of Both Worlds.</h2>
+    <p>{{ siteData.combination }}</p>
+    <div class="bennies">
+      <div class="benefits">
+        <h3>Static Sites</h3>
+        <hr>
+        <ul>
+          <li v-for="benefit in siteData.staticBenefits">{{ benefit | capitalize }}</li>
+        </ul>
+      </div>
+      <div class="benefits">
+        <h3>Client-Side JS</h3>
+        <hr>
+        <ul>
+          <li v-for="benefit in siteData.dynamicBenefits">{{ benefit | capitalize }}</li>
+          <li v-for="item in newDynamic">{{ item }}</li>
+          <li><input label="More great things about static sites"type="" name="" v-model="newItem">
+        <button v-on:click="addItem" placeholder="Another benefit">Add</button></li>
+        </ul>
+      </div>
+    </div>
+      <p class="environment">*Disable Javascript to see the page rendered with Jekyll.</p>
+  </div>
+</template> -->
+
+<script>
+import siteData from '../_site/data/data.json'
+import Marked from 'marked'
+export default {
+  data () {
+
+    return {
+      siteData: siteData,
+      newItem: 'Data binding',
+      newDynamic: []
+    }
+  },
+    filters: {
+      capitalize: function (value) {
+        if (!value) return ''
+        value = value.toString()
+        return value.charAt(0).toUpperCase() + value.slice(1)
+      },
+      md: function (val) {
+          return Marked(val)
+      }
+  },
+  computed: {
+    rawdescription: function(){
+      return Marked(siteData.description)
+    }
+  },
+  methods: {
+    addItem() {
+      this.newDynamic.push(this.newItem);
+      this.newItem = '';
+    },
+    enter: function(el,done){
+      debugger;
+      console.log("I wss here")
+      // done();
+    }
+  }
+}
+</script>
--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
 ---
-layout: 
+layout:
 sitemap:
   exclude: 'yes'
 ---
-
-$(document).ready(function () {
+$(document).ready(function(){
   $('a.blog-button').click(function (e) {
-    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
-    currentWidth = $('.panel-cover').width()
-    if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed')
-      $('.content-wrapper').addClass('animated slideInRight')
-    } else {
-      $('.panel-cover').css('max-width', currentWidth)
-      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+    if ($('.panel-cover').hasClass('panel-cover--collapsed')){
+      var currentWidth = $('body').width()
+      $('.panel-cover').animate({'max-width': currentWidth, 'width': '100%'}, 400, swing = 'swing', function () {})
+    }else{
+      var currentWidth = $('.panel-cover').width()
+      if (currentWidth < 960) {
+        $('.content-wrapper').addClass('animated slideInRight')
+      } else {
+        $('.panel-cover').css('max-width', currentWidth)
+        $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+      }
     }
+    $('.panel-cover').toggleClass('panel-cover--collapsed')
+
   })
 
   if (window.location.hash && window.location.hash == '#blog') {
@@ -34,5 +38,92 @@ $(document).ready(function () {
     $('.navigation-wrapper').toggleClass('visible')
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
   })
-
 })
+
+
+
+
+// $(document).ready(function () {
+//   $('a.blog-button').click(function (e) {
+//     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
+//     currentWidth = $('.panel-cover').width()
+//     if (currentWidth < 960) {
+//       $('.panel-cover').addClass('panel-cover--collapsed')
+//       $('.content-wrapper').addClass('animated slideInRight')
+//     } else {
+//       $('.panel-cover').css('max-width', currentWidth)
+//       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+//     }
+//   })
+//
+//   if (window.location.hash && window.location.hash == '#blog') {
+//     $('.panel-cover').addClass('panel-cover--collapsed')
+//   }
+//
+//   if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
+//     $('.panel-cover').addClass('panel-cover--collapsed')
+//   }
+//
+//   $('.btn-mobile-menu').click(function () {
+//     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
+//     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+//   })
+//
+//   $('.navigation-wrapper .blog-button').click(function () {
+//     $('.navigation-wrapper').toggleClass('visible')
+//     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+//   })
+//
+// })
+
+
+
+
+
+
+
+//
+// $(document).ready(function () {
+// var canvas = document.getElementById('myCanvas');
+// var ctx = canvas.getContext('2d');
+//
+// var sketch = document.querySelector("#dd");;
+// var sketch = document.querySelector("#dd");;
+// canvas.width = parseInt(sketch_style.getPropertyValue('width'));
+// canvas.height = parseInt(sketch_style.getPropertyValue('height'));
+//   var mouse = {x: 0, y: 0};
+//     var last_mouse = {x: 0, y: 0};
+//
+//     /* Mouse Capturing Work */
+//     canvas.addEventListener('mousemove', function(e) {
+//         last_mouse.x = mouse.x;
+//         last_mouse.y = mouse.y;
+//
+//         mouse.x = e.pageX - this.offsetLeft;
+//         mouse.y = e.pageY - this.offsetTop;
+//     }, false);
+//
+//     /* Drawing on Paint App */
+//     ctx.lineWidth = 5;
+//     ctx.lineJoin = 'round';
+//     ctx.lineCap = 'round';
+//     ctx.strokeStyle = 'blue';
+//
+//     canvas.addEventListener('mousedown', function(e) {
+//         canvas.addEventListener('mousemove', onPaint, false);
+//     }, false);
+//
+//     canvas.addEventListener('mouseup', function() {
+//         canvas.removeEventListener('mousemove', onPaint, false);
+//     }, false);
+//
+//     var onPaint = function() {
+//         ctx.beginPath();
+//         ctx.moveTo(last_mouse.x, last_mouse.y);
+//         ctx.lineTo(mouse.x, mouse.y);
+//         ctx.closePath();
+//         ctx.stroke();
+//     };
+//
+//
+// });
